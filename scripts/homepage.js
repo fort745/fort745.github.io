12c58c33985deaefd456589352de20a09a62fa83
iris_li <irisli12345@gmail.com>
2020-06-06 00:37:36 -0400

    added some stuff to make mobile navigation easier

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,34 +1,35 @@
 //function to select html elements
 const selectElement = (s) => document.querySelector(s);
 //opens menu on click
-/*
+
 let open = false;
-selectElement(".menu-icon").addEventListener("click", () => {
+selectElement(".nav-group").addEventListener("click", () => {
     if(open){
-        selectElement(".container").classList.remove("active");
+        selectElement(".nav-group").classList.remove("active");
         //change icon to down arrow
         open = false;
     } else {
-        selectElement(".container").classList.add("active");
+        selectElement(".nav-group").classList.add("active");
         //change icon to up arrow
         open = true;
     }
 });
-//on scroll also close menu
+//on scroll also close menu (helps for mobile)
 window.addEventListener("scroll", () => {
     if(open){
-        selectElement(".container").classList.remove("active");
+        selectElement(".nav-group").classList.remove("active");
         //change icon to down arrow
         open = false;
     }
 });
-*/
 
 selectElement(".nav-group").addEventListener("mouseenter", () => {
     selectElement(".nav-group").classList.add("active");
         //change icon to up arrow
+        open = true;
 });
 selectElement(".nav-group").addEventListener("mouseleave", () => {
     selectElement(".nav-group").classList.remove("active");
         //change icon to up arrow
+        open = false;
 });
